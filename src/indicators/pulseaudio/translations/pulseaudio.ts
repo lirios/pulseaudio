8a7618147f01cd18090330ea81a769358c3b60ec
Pier Luigi Fiorini <pierluigi.fiorini@liri.io>
2018-05-26 12:04:23 +0200

    Add indicator to translations

---
+++ src/indicators/pulseaudio/translations/pulseaudio.ts
@@ -0,0 +1,65 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>MprisItem</name>
+    <message>
+        <location filename="../contents/MprisItem.qml" line="80"/>
+        <source>Unknown Title</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../contents/MprisItem.qml" line="91"/>
+        <source>Unknown Artist</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../contents/MprisItem.qml" line="100"/>
+        <source>Previous</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../contents/MprisItem.qml" line="112"/>
+        <source>Pause</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../contents/MprisItem.qml" line="112"/>
+        <source>Play</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../contents/MprisItem.qml" line="124"/>
+        <source>Next</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../contents/main.qml" line="35"/>
+        <source>Sound</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../contents/main.qml" line="38"/>
+        <source>Muted</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../contents/main.qml" line="39"/>
+        <source>Volume at %1%</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../contents/main.qml" line="47"/>
+        <source>Volume</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../contents/main.qml" line="57"/>
+        <source>Music</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
