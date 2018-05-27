07c4bc8726131e4e96cd761b8003eac2014a78af
Pier Luigi Fiorini <pierluigi.fiorini@liri.io>
2018-05-27 13:52:49 +0200

    Commit new translations from CI

--- scripts/txpull.sh
+++ scripts/txpull.sh
@@ -2,3 +2,16 @@
 
 # Fetch translations
 tx pull --force --all --minimum-perc=5
+
+# Commit to git
+if [ "$CI" = "true" ]; then
+    AUTHOR="Liri CI <ci@liri.io>"
+    SUBJECT="Automatic merge of Transifex translations"
+    git config --global push.default simple
+    git add --verbose src/indicators/pulseaudio/translations/pulseaudio_*.ts
+    git add --verbose src/indicators/pulseaudio/translations/metadata_*.desktop
+    git add --verbose src/settings/pulseaudio/translations/pulseaudio_*.ts
+    git add --verbose src/settings/pulseaudio/translations/metadata_*.desktop
+    git commit --author="$AUTHOR" --message="$SUBJECT"
+    git push
+fi
