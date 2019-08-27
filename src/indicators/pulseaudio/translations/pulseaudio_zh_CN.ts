b3a0fc975e2768e831ad3ca85d64f06974cb8a7e
Liri CI <ci@liri.io>
2019-08-27 02:45:55 +0000

    Automatic merge of Transifex translations
    [ci skip]

---
+++ src/indicators/pulseaudio/translations/pulseaudio_zh_CN.ts
@@ -0,0 +1,63 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="zh_CN" version="2.1">
+<context>
+    <name>MprisItem</name>
+    <message>
+        <location filename="../contents/MprisItem.qml" line="80"/>
+        <source>Unknown Title</source>
+        <translation>未知标题</translation>
+    </message>
+    <message>
+        <location filename="../contents/MprisItem.qml" line="91"/>
+        <source>Unknown Artist</source>
+        <translation>未知作者</translation>
+    </message>
+    <message>
+        <location filename="../contents/MprisItem.qml" line="100"/>
+        <source>Previous</source>
+        <translation>上一首</translation>
+    </message>
+    <message>
+        <location filename="../contents/MprisItem.qml" line="112"/>
+        <source>Pause</source>
+        <translation>暂停</translation>
+    </message>
+    <message>
+        <location filename="../contents/MprisItem.qml" line="112"/>
+        <source>Play</source>
+        <translation>播放</translation>
+    </message>
+    <message>
+        <location filename="../contents/MprisItem.qml" line="124"/>
+        <source>Next</source>
+        <translation>下一首</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../contents/main.qml" line="34"/>
+        <source>Sound</source>
+        <translation>声音</translation>
+    </message>
+    <message>
+        <location filename="../contents/main.qml" line="37"/>
+        <source>Muted</source>
+        <translation>静音</translation>
+    </message>
+    <message>
+        <location filename="../contents/main.qml" line="38"/>
+        <source>Volume at %1%</source>
+        <translation>音量： %1%</translation>
+    </message>
+    <message>
+        <location filename="../contents/main.qml" line="46"/>
+        <source>Volume</source>
+        <translation>音量</translation>
+    </message>
+    <message>
+        <location filename="../contents/main.qml" line="56"/>
+        <source>Music</source>
+        <translation>音乐</translation>
+    </message>
+</context>
+</TS>
--- src/settings/pulseaudio/translations/pulseaudio_zh_CN.ts
+++ src/settings/pulseaudio/translations/pulseaudio_zh_CN.ts
@@ -1,33 +1,10 @@
 <?xml version="1.0" ?><!DOCTYPE TS><TS language="zh_CN" version="2.1">
 <context>
-    <name>Preflet</name>
-    <message>
-        <location filename="../contents/Preflet.qml" line="47"/>
-        <source>Output</source>
-        <translation>输出</translation>
-    </message>
-    <message>
-        <location filename="../contents/Preflet.qml" line="55"/>
-        <source>Applications</source>
-        <translation type="unfinished"/>
-    </message>
-    <message>
-        <location filename="../contents/Preflet.qml" line="43"/>
-        <source>Volume</source>
-        <translation>音量</translation>
-    </message>
-    <message>
-        <location filename="../contents/Preflet.qml" line="51"/>
-        <source>Input</source>
-        <translation>输入</translation>
-    </message>
-</context>
-<context>
     <name>StreamDelegate</name>
     <message>
         <location filename="../contents/StreamDelegate.qml" line="59"/>
         <source>Alerts</source>
-        <translation type="unfinished"/>
+        <translation>提示</translation>
     </message>
 </context>
 <context>
@@ -35,7 +12,30 @@
     <message>
         <location filename="../contents/VolumeSlider.qml" line="64"/>
         <source>100%</source>
-        <translation type="unfinished"/>
+        <translation>100%</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../contents/main.qml" line="43"/>
+        <source>Volume</source>
+        <translation>音量</translation>
+    </message>
+    <message>
+        <location filename="../contents/main.qml" line="47"/>
+        <source>Output</source>
+        <translation>输出</translation>
+    </message>
+    <message>
+        <location filename="../contents/main.qml" line="51"/>
+        <source>Input</source>
+        <translation>输入</translation>
+    </message>
+    <message>
+        <location filename="../contents/main.qml" line="55"/>
+        <source>Applications</source>
+        <translation>应用程序</translation>
     </message>
 </context>
 </TS>
