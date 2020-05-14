d4c4ddbfa2d5cd86c8d400ae91ef368ee66ce3c6
Liri CI <ci@liri.io>
2020-05-14 00:12:21 +0000

    Automatic merge of Transifex translations
    [ci skip]

---
+++ translations/indicator/pulseaudio_it_IT.ts
@@ -0,0 +1,63 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="it_IT" version="2.1">
+<context>
+    <name>MprisItem</name>
+    <message>
+        <location filename="../../src/indicators/pulseaudio/contents/MprisItem.qml" line="80"/>
+        <source>Unknown Title</source>
+        <translation>Titolo sconosciuto</translation>
+    </message>
+    <message>
+        <location filename="../../src/indicators/pulseaudio/contents/MprisItem.qml" line="91"/>
+        <source>Unknown Artist</source>
+        <translation>Artista sconosciuto</translation>
+    </message>
+    <message>
+        <location filename="../../src/indicators/pulseaudio/contents/MprisItem.qml" line="100"/>
+        <source>Previous</source>
+        <translation>Precedente</translation>
+    </message>
+    <message>
+        <location filename="../../src/indicators/pulseaudio/contents/MprisItem.qml" line="112"/>
+        <source>Pause</source>
+        <translation>Pausa</translation>
+    </message>
+    <message>
+        <location filename="../../src/indicators/pulseaudio/contents/MprisItem.qml" line="112"/>
+        <source>Play</source>
+        <translation>Esegui</translation>
+    </message>
+    <message>
+        <location filename="../../src/indicators/pulseaudio/contents/MprisItem.qml" line="124"/>
+        <source>Next</source>
+        <translation>Successivo</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../../src/indicators/pulseaudio/contents/main.qml" line="34"/>
+        <source>Sound</source>
+        <translation>Suono</translation>
+    </message>
+    <message>
+        <location filename="../../src/indicators/pulseaudio/contents/main.qml" line="37"/>
+        <source>Muted</source>
+        <translation>Muto</translation>
+    </message>
+    <message>
+        <location filename="../../src/indicators/pulseaudio/contents/main.qml" line="38"/>
+        <source>Volume at %1%</source>
+        <translation>Volume al %1%</translation>
+    </message>
+    <message>
+        <location filename="../../src/indicators/pulseaudio/contents/main.qml" line="46"/>
+        <source>Volume</source>
+        <translation>Volume</translation>
+    </message>
+    <message>
+        <location filename="../../src/indicators/pulseaudio/contents/main.qml" line="56"/>
+        <source>Music</source>
+        <translation>Musica</translation>
+    </message>
+</context>
+</TS>
