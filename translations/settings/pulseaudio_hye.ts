d69f7f134bdab5688cb4b41035fecd19eaa5904e
Liri CI <ci@liri.io>
2020-06-27 00:18:37 +0000

    Automatic merge of Transifex translations
    [ci skip]

---
+++ translations/settings/pulseaudio_hye.ts
@@ -0,0 +1,41 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="hye" version="2.1">
+<context>
+    <name>StreamDelegate</name>
+    <message>
+        <location filename="../../src/settings/pulseaudio/contents/StreamDelegate.qml" line="59"/>
+        <source>Alerts</source>
+        <translation>Զգուշացումներ</translation>
+    </message>
+</context>
+<context>
+    <name>VolumeSlider</name>
+    <message>
+        <location filename="../../src/settings/pulseaudio/contents/VolumeSlider.qml" line="64"/>
+        <source>100%</source>
+        <translation>100%</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../../src/settings/pulseaudio/contents/main.qml" line="43"/>
+        <source>Volume</source>
+        <translation>Ձայնի բարձութիւն</translation>
+    </message>
+    <message>
+        <location filename="../../src/settings/pulseaudio/contents/main.qml" line="47"/>
+        <source>Output</source>
+        <translation>Արտածում</translation>
+    </message>
+    <message>
+        <location filename="../../src/settings/pulseaudio/contents/main.qml" line="51"/>
+        <source>Input</source>
+        <translation>Ներածում</translation>
+    </message>
+    <message>
+        <location filename="../../src/settings/pulseaudio/contents/main.qml" line="55"/>
+        <source>Applications</source>
+        <translation>Յաւելուածներ</translation>
+    </message>
+</context>
+</TS>
