6565f0a1200e520478acb018b08641805777853e
Liri CI <ci@liri.io>
2020-10-23 00:37:53 +0000

    Automatic merge of Transifex translations
    [ci skip]

--- translations/indicator/pulseaudio_he.ts
+++ translations/indicator/pulseaudio_he.ts
@@ -4,27 +4,27 @@
     <message>
         <location filename="../../src/indicators/pulseaudio/contents/MprisItem.qml" line="80"/>
         <source>Unknown Title</source>
-        <translation type="unfinished"/>
+        <translation>כותר לא ידוע</translation>
     </message>
     <message>
         <location filename="../../src/indicators/pulseaudio/contents/MprisItem.qml" line="91"/>
         <source>Unknown Artist</source>
-        <translation type="unfinished"/>
+        <translation>אמן לא ידוע</translation>
     </message>
     <message>
         <location filename="../../src/indicators/pulseaudio/contents/MprisItem.qml" line="100"/>
         <source>Previous</source>
-        <translation type="unfinished"/>
+        <translation>הקודם</translation>
     </message>
     <message>
         <location filename="../../src/indicators/pulseaudio/contents/MprisItem.qml" line="112"/>
         <source>Pause</source>
-        <translation type="unfinished"/>
+        <translation>השהייה</translation>
     </message>
     <message>
         <location filename="../../src/indicators/pulseaudio/contents/MprisItem.qml" line="112"/>
         <source>Play</source>
-        <translation type="unfinished"/>
+        <translation>השמעה</translation>
     </message>
     <message>
         <location filename="../../src/indicators/pulseaudio/contents/MprisItem.qml" line="124"/>
@@ -37,27 +37,27 @@
     <message>
         <location filename="../../src/indicators/pulseaudio/contents/main.qml" line="34"/>
         <source>Sound</source>
-        <translation type="unfinished"/>
+        <translation>שמע</translation>
     </message>
     <message>
         <location filename="../../src/indicators/pulseaudio/contents/main.qml" line="37"/>
         <source>Muted</source>
-        <translation type="unfinished"/>
+        <translation>השתקה</translation>
     </message>
     <message>
         <location filename="../../src/indicators/pulseaudio/contents/main.qml" line="38"/>
         <source>Volume at %1%</source>
-        <translation type="unfinished"/>
+        <translation>עוצמה ב %1%</translation>
     </message>
     <message>
         <location filename="../../src/indicators/pulseaudio/contents/main.qml" line="46"/>
         <source>Volume</source>
-        <translation type="unfinished"/>
+        <translation>עוצמה</translation>
     </message>
     <message>
         <location filename="../../src/indicators/pulseaudio/contents/main.qml" line="56"/>
         <source>Music</source>
-        <translation type="unfinished"/>
+        <translation>מוזיקה</translation>
     </message>
 </context>
 </TS>
