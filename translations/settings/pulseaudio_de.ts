70be0e39853253beed914907c1157ce776905ef8
Liri CI <ci@liri.io>
2021-08-10 01:00:44 +0000

    Automatic merge of Transifex translations
    [ci skip]

--- translations/settings/pulseaudio_de.ts
+++ translations/settings/pulseaudio_de.ts
@@ -1,41 +1,41 @@
 <?xml version="1.0" ?><!DOCTYPE TS><TS language="de" version="2.1">
 <context>
-    <name>Preflet</name>
-    <message>
-        <location filename="../contents/Preflet.qml" line="47"/>
-        <source>Output</source>
-        <translation>Output</translation>
-    </message>
-    <message>
-        <location filename="../contents/Preflet.qml" line="55"/>
-        <source>Applications</source>
-        <translation type="unfinished"/>
-    </message>
-    <message>
-        <location filename="../contents/Preflet.qml" line="43"/>
-        <source>Volume</source>
-        <translation>Lautstärke</translation>
-    </message>
-    <message>
-        <location filename="../contents/Preflet.qml" line="51"/>
-        <source>Input</source>
-        <translation>Input</translation>
-    </message>
-</context>
-<context>
     <name>StreamDelegate</name>
     <message>
-        <location filename="../contents/StreamDelegate.qml" line="59"/>
+        <location filename="../../src/settings/pulseaudio/contents/StreamDelegate.qml" line="59"/>
         <source>Alerts</source>
-        <translation type="unfinished"/>
+        <translation>Warnhinweise</translation>
     </message>
 </context>
 <context>
     <name>VolumeSlider</name>
     <message>
-        <location filename="../contents/VolumeSlider.qml" line="64"/>
+        <location filename="../../src/settings/pulseaudio/contents/VolumeSlider.qml" line="64"/>
         <source>100%</source>
-        <translation type="unfinished"/>
+        <translation>100 %</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../../src/settings/pulseaudio/contents/main.qml" line="43"/>
+        <source>Volume</source>
+        <translation>Lautstärke</translation>
+    </message>
+    <message>
+        <location filename="../../src/settings/pulseaudio/contents/main.qml" line="47"/>
+        <source>Output</source>
+        <translation>Ausgang</translation>
+    </message>
+    <message>
+        <location filename="../../src/settings/pulseaudio/contents/main.qml" line="51"/>
+        <source>Input</source>
+        <translation>Eingang</translation>
+    </message>
+    <message>
+        <location filename="../../src/settings/pulseaudio/contents/main.qml" line="55"/>
+        <source>Applications</source>
+        <translation>Anwendungen</translation>
     </message>
 </context>
 </TS>
---
+++ translations/statusarea/pulseaudio_de.ts
@@ -0,0 +1,49 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="de" version="2.1">
+<context>
+    <name>MprisItem</name>
+    <message>
+        <location filename="../../src/statusarea/pulseaudio/contents/MprisItem.qml" line="61"/>
+        <source>Unknown Title</source>
+        <translation>Unbekannter Titel</translation>
+    </message>
+    <message>
+        <location filename="../../src/statusarea/pulseaudio/contents/MprisItem.qml" line="72"/>
+        <source>Unknown Artist</source>
+        <translation>Unbekannter Künstler</translation>
+    </message>
+    <message>
+        <location filename="../../src/statusarea/pulseaudio/contents/MprisItem.qml" line="81"/>
+        <source>Previous</source>
+        <translation>Vorheriger</translation>
+    </message>
+    <message>
+        <location filename="../../src/statusarea/pulseaudio/contents/MprisItem.qml" line="93"/>
+        <source>Pause</source>
+        <translation>Pause</translation>
+    </message>
+    <message>
+        <location filename="../../src/statusarea/pulseaudio/contents/MprisItem.qml" line="93"/>
+        <source>Play</source>
+        <translation>Wiedergabe</translation>
+    </message>
+    <message>
+        <location filename="../../src/statusarea/pulseaudio/contents/MprisItem.qml" line="105"/>
+        <source>Next</source>
+        <translation>Nächster</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../../src/statusarea/pulseaudio/contents/main.qml" line="70"/>
+        <source>Sound</source>
+        <translation>Klang</translation>
+    </message>
+    <message>
+        <location filename="../../src/statusarea/pulseaudio/contents/main.qml" line="52"/>
+        <location filename="../../src/statusarea/pulseaudio/contents/main.qml" line="84"/>
+        <source>Music</source>
+        <translation>Musik</translation>
+    </message>
+</context>
+</TS>
